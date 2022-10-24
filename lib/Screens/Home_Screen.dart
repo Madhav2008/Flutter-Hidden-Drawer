618889fe576e8c271a6149d4196a959f610b2e6c
Madhav2008 <aroramadhav65@gmail.com>
2022-10-24 23:52:25 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -11,7 +11,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(data),
+      appBar: AppBar(
+        title: Text(data),
         backgroundColor: Colors.deepPurple,
       ),
     );
