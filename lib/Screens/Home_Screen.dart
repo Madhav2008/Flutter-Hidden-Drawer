b5387d4b52fac1629257a9c237c9630ee2dff59a
Madhav2008 <aroramadhav65@gmail.com>
2022-10-24 23:53:32 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -17,7 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(
             letterSpacing: 2,
           ),
-        ),
+        ),centerTitle: null,
         backgroundColor: Colors.deepPurple,
       ),
     );
