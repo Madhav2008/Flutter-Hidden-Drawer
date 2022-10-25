1c4e59bb1cbe955586aa96b5a2d7993cce5fae00
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 14:20:28 +0530

    Update

--- lib/Hidden_Drawer.dart
+++ lib/Hidden_Drawer.dart
@@ -22,7 +22,7 @@ class _HiddenDrawerState extends State<HiddenDrawer> {
       ScreenHiddenDrawer(
         ItemHiddenMenu(
           name: 'HOMESCREEN',
-          baseStyle: TextStyle(),
+          baseStyle: TextStyle(letterSpacing: 2,),
           selectedStyle: TextStyle(),
         ),
         const HomeScreen(),
