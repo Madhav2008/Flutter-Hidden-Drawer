4ac520b2c0b791ea3e129be98c8716be545c1706
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 13:59:20 +0530

    Update

---
+++ lib/Hidden_Drawer.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      appBar: AppBar(
+        title: const Text(
+          'HOMEPAGE',
+          style: TextStyle(
+            letterSpacing: 2,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+}
