6a42ef7551bba05bd6ebc9795f63624db9572b10
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 13:59:44 +0530

    Update

--- lib/Hidden_Drawer.dart
+++ lib/Hidden_Drawer.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,18 +13,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple[200],
-      appBar: AppBar(
-        title: const Text(
-          'HOMEPAGE',
-          style: TextStyle(
-            letterSpacing: 2,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-      ),
-    );
+    return HiddenDrawerMenu(screens: screens, backgroundColorMenu: backgroundColorMenu);
   }
 }
