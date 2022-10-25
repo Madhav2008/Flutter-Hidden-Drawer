0868dcaf181b30898f38e16f0bc74d9a62576508
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 14:15:46 +0530

    Update

--- lib/Hidden_Drawer.dart
+++ lib/Hidden_Drawer.dart
@@ -3,14 +3,14 @@
 import 'package:flutter/material.dart';
 import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HiddenDrawer extends StatefulWidget {
+  const HiddenDrawer({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HiddenDrawer> createState() => _HiddenDrawerState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HiddenDrawerState extends State<HiddenDrawer> {
   List<ScreenHiddenDrawer> _screens = [];
 
   @override
