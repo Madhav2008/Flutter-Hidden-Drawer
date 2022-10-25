83a96f518afcbc5d65ddb6c2b1e4d54095359eea
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 14:15:07 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HiddenDrawerMenu(),
+      home: HiddenDrawer(),
     );
   }
 }
