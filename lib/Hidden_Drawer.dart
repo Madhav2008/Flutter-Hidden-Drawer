cf83dbc450ff339a84f4fc1890e9bcf87af2985a
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 13:59:48 +0530

    Update

--- lib/Hidden_Drawer.dart
+++ lib/Hidden_Drawer.dart
@@ -13,6 +13,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return HiddenDrawerMenu(screens: screens, backgroundColorMenu: backgroundColorMenu);
+    return HiddenDrawerMenu(screens: [], backgroundColorMenu: backgroundColorMenu);
   }
 }
