845802cf77feb99ae5a92800ff56218db746a4f7
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 14:33:43 +0530

    Update

--- lib/Hidden_Drawer.dart
+++ lib/Hidden_Drawer.dart
@@ -52,7 +52,8 @@ class _HiddenDrawerState extends State<HiddenDrawer> {
       backgroundColorAppBar: Colors.deepPurple,
       screens: _screens,
       isTitleCentered: true,
-      initPositionSelected: 0,slidePercent: 60,
+      initPositionSelected: 0,
+      slidePercent: 60,
     );
   }
 }
